3630ea060e7b9051b094044764823d225f75152a
Mustakim <sycospike1@gmail.com>
2023-02-26 23:42:14 +0600

    added keyup enter button

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
-const loadPhones = async(searchText) =>{
+const loadPhones = async(searchText, dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
      
 }
 
-const displayPhones = phones =>{
+const displayPhones = (phones, dataLimit) =>{
    const phonesContainer = document.getElementById('phone-container');
    phonesContainer.innerText = ' ' ;
-   // display 20 phones only
-   phones = phones.slice(0,20);
+   // display 10 phones only
+   const showAll = document.getElementById('show-all');
+   if(dataLimit && phones.length > 10 ){
+    phones = phones.slice(0,10);
+    showAll.classList.remove('d-none');
+   }
+   else{
+    showAll.classList.add('d-none');
+   }
+   
 
    // display no phones found
    const noPhone = document.getElementById('no-found-message');
@@ -30,6 +38,7 @@ const displayPhones = phones =>{
     <div class="card-body">
       <h5 class="card-title">${phone.phone_name}</h5>
       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
     </div>
   </div>
     `;
@@ -39,14 +48,26 @@ const displayPhones = phones =>{
    toggleSpinner(false);
 }
 
-// handle search button click
-document.getElementById('btn-search').addEventListener('click',function(){
-  // start loader
+const processSearch = (dataLimit) =>{
   toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText);
+    loadPhones(searchText,dataLimit);
+}
+
+// handle search button click
+document.getElementById('btn-search').addEventListener('click',function(){
+   // start loader
+   processSearch(10);
 })
+
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+  //console.log(e.key);
+  if (e.key === 'Enter') {
+    processSearch(10);
+  }
+});
 
 const toggleSpinner = isLoading => {
   const loaderSection = document.getElementById('loader');
@@ -58,4 +79,17 @@ const toggleSpinner = isLoading => {
   }
 }
 
+// not the best way to load show all
+document.getElementById('btn-show-all').addEventListener('click',function(){
+  processSearch();
+})
+
+const loadPhoneDetails = async id =>{
+  const url =` https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+
+}
 //loadPhone();
+
